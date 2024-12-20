b5c8d19a5414854a551fa79704d2b8ea71a3683a
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:12:37 +0300

    Implement functionality to list all contacts

--- cli.py
+++ cli.py
@@ -4,3 +4,11 @@ def add_contact():
     email = input("Enter email: ").strip()
     Contact.add_contact(name, phone_number, email) 
     print("Contact added successfully.")
+
+
+def list_contacts():
+    contacts = Contact.get_all_contacts()
+    print("Contacts:")
+    for contact in contacts:
+        contact_id, name, phone_number, email = contact
+        print(f"ID: {contact_id} | Name: {name} | Phone: {phone_number} | Email: {email}")
