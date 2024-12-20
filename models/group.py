8f0e5a0342ee237eb1a6264988bfd0f8a57e580f
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:33:29 +0300

    Implement functionality to search for a group by ID

--- models/group.py
+++ models/group.py
@@ -30,3 +30,10 @@ class Group:
         db = Database()
         db.cursor.execute("SELECT * FROM groups")
         return db.cursor.fetchall()
+
+    @classmethod
+    def find_group_by_id(cls, group_id):
+        db = Database()
+        db.cursor.execute("SELECT * FROM groups WHERE id = ?", (group_id,))
+        return db.cursor.fetchone()
+
