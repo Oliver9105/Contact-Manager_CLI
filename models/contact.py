e821cfedb916ab752e4f2ef9d949c5927977d6ee
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:08:27 +0300

    Add attributes to Contact class

--- models/contact.py
+++ models/contact.py
@@ -1,4 +1,8 @@
 from database import Database
 
 class Contact:
-    pass 
+     def __init__(self, name, phone_number, email): 
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+   
