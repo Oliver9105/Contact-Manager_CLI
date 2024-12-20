7827b533c03a885f645a7758eea068d9c61faa55
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:09:39 +0300

    Create table in the database to store contact information

--- models/contact.py
+++ models/contact.py
@@ -1,8 +1,20 @@
 from database import Database
 
 class Contact:
-     def __init__(self, name, phone_number, email): 
+    def __init__(self, name, phone_number, email): 
         self.name = name
         self.phone_number = phone_number
         self.email = email
-   
+
+    @classmethod
+    def create_table(cls):
+        db = Database()
+        db.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                phone_number TEXT NOT NULL,
+                email TEXT NOT NULL
+            )
+        """)
+        db.conn.commit()
