68acfad1b1d0224d26a25d53dc09aeb0d2863378
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:39:51 +0300

    Define ContactGroup class with methods to manage contact-group relationships

--- models/contact_group.py
+++ models/contact_group.py
@@ -15,3 +15,42 @@ class ContactGroup:
             )
         """)
         db.conn.commit()
+
+    @classmethod
+    def assign_contact_to_group(cls, contact_id, group_id):
+        db = Database()
+        db.cursor.execute("""
+            INSERT INTO contact_group (contact_id, group_id)
+            VALUES (?, ?)
+        """, (contact_id, group_id))
+        db.conn.commit()
+
+    @classmethod
+    def get_groups_for_contact(cls, contact_id):
+        db = Database()
+        db.cursor.execute("""
+            SELECT g.name 
+            FROM groups g
+            JOIN contact_group cg ON g.id = cg.group_id 
+            WHERE cg.contact_id = ?
+        """, (contact_id,))
+        return db.cursor.fetchall()
+
+    @classmethod
+    def get_contacts_for_group(cls, group_id):
+        db = Database()
+        db.cursor.execute("""
+            SELECT c.name
+            FROM contacts c
+            JOIN contact_group cg ON c.id = cg.contact_id
+            WHERE cg.group_id = ?
+        """, (group_id,))
+        return db.cursor.fetchall()
+
+    @classmethod
+    def remove_contact_from_group(cls, contact_id, group_id):
+        db = Database()
+        db.cursor.execute("""
+            DELETE FROM contact_group WHERE contact_id = ? AND group_id = ?
+        """, (contact_id, group_id))
+        db.conn.commit()
