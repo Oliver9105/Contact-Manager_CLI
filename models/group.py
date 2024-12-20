2ca2488dd5c305e170d249d16192ed90ae97d44f
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 20:24:08 +0300

    Create table in the database to store group information

--- models/group.py
+++ models/group.py
@@ -4,3 +4,14 @@ from database import Database
 class Group:
     def __init__(self, name):
         self.name = name
+
+    @classmethod
+    def create_table(cls):
+        db = Database()
+        db.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS groups (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+        """)
+        db.conn.commit()
