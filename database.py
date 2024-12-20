feb69872548bc93604b61496551d62b0531da065
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-20 21:05:55 +0300

    Fix incorrect database file path

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
 import sqlite3
+import os
 
 class Database:
-  def __init__(self):
-    self.conn = sqlite3.connect("contacts.db")
-    self.cursor = self.conn.cursor()
+    def __init__(self, db_file="db/contacts.db"): 
+        db_folder = os.path.dirname(db_file) 
+        if not os.path.exists(db_folder):
+            os.makedirs(db_folder)
 
-  def close(self):
-    self.conn.close()
+        self.conn = sqlite3.connect(db_file) 
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
