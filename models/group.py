e1baa5068251990dd567e19154f5cc2f7fcc4e02
Oliver cheruiyot <olivercher1000@gmail.com>
2025-01-09 12:24:04 +0300

    Update Group model with properties and DB methods

--- models/group.py
+++ models/group.py
@@ -1,45 +1,47 @@
-# models/group.py
 from database import Database
 
 class Group:
-    def __init__(self, name):
+    def __init__(self, group_id, name):
+        self.id = group_id
         self.name = name
 
     @classmethod
     def create_table(cls):
         db = Database()
-        db.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS groups (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL
-            )
-        """)
-        db.conn.commit()
+        query = """
+        CREATE TABLE IF NOT EXISTS groups (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+        """
+        db.execute_query(query)
 
     @classmethod
     def add_group(cls, name):
         db = Database()
-        db.cursor.execute("""
-            INSERT INTO groups (name)
-            VALUES (?)
-        """, (name,))
-        db.conn.commit()
+        query = "INSERT INTO groups (name) VALUES (?)"
+        db.execute_query(query, (name,))
 
     @classmethod
     def get_all_groups(cls):
         db = Database()
-        db.cursor.execute("SELECT * FROM groups")
-        return db.cursor.fetchall()
+        query = "SELECT * FROM groups"
+        rows = db.execute_query(query)
+        return [cls.from_db_row(row) for row in rows]  # Use from_db_row method
 
     @classmethod
     def find_group_by_id(cls, group_id):
         db = Database()
-        db.cursor.execute("SELECT * FROM groups WHERE id = ?", (group_id,))
-        return db.cursor.fetchone()
+        query = "SELECT * FROM groups WHERE id = ?"
+        rows = db.execute_query(query, (group_id,))
+        return cls.from_db_row(rows[0]) if rows else None  # Use from_db_row method
 
     @classmethod
-    def delete_group(cls, group_name):
+    def delete_group(cls, name):
         db = Database()
-        db.cursor.execute("DELETE FROM groups WHERE name = ?", (group_name,))
-        db.conn.commit()
+        query = "DELETE FROM groups WHERE name = ?"
+        db.execute_query(query, (name,))
 
+    @classmethod
+    def from_db_row(cls, row):
+        return cls(row[0], row[1])
